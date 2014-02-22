741eafa431ba2d461a738c52b312351e9a0ce914
RM <rrmm@users.noreply.github.com>
2014-02-22 17:29:39 -0500

    initial creation of preseed and software setup

---
+++ config/installsoftware.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+
+echo "manual" > /etc/init/bluetooth.override
+echo "manual" > /etc/init/cups.override
+echo "manual" > /etc/init/lightdm.override
+add-apt-repository ppa:x2go/stable
+add-apt-repository "http://neuro.debian.net/debian main contrib non-free"
+apt-key adv --recv-keys --keyserver pgp.mit.edu 2649A5A9
+apt-get update
+apt-get -y install emacs openssh-server x2goserver x2golxdebindings lxde-common fsl fslview afni itksnap mricron spm8-common spm8-data spm8-doc ia32-libs libc6-i386 galternatives midori qt4-qtconfig mesa-utils geany
+apt-get -y remove update-notifier-core
+
