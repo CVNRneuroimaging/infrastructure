7e7261dc52c368aced7f10749d81754417215022
Stephen Towler <stowler@gmail.com>
2014-02-26 11:54:41 -0500

    minor organizing change
    added sudo (to discourage root use), added aptitude package, segregated by role

--- config/installsoftware.sh
+++ config/installsoftware.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 
 
-
 echo "manual" > /etc/init/bluetooth.override
 echo "manual" > /etc/init/cups.override
 echo "manual" > /etc/init/lightdm.override
+
+# add non-X system packages:
+sudo apt-get -y install ia32-libs libc6-i386 aptitude emacs vim openssh-server
+
+# add GUI system packages:
+sudo apt-get -y install lxde-common galternatives  x2goserver x2golxdebindings midori qt4-qtconfig mesa-utils geany
+
+# add neuroimaging repos:
 add-apt-repository ppa:x2go/stable
 add-apt-repository "http://neuro.debian.net/debian main contrib non-free"
 add-apt-repository "deb http://neuro.debian.net/debian data main contrib non-free"
 apt-key adv --recv-keys --keyserver pgp.mit.edu 2649A5A9
 apt-get update
-apt-get -y install emacs openssh-server x2goserver x2golxdebindings lxde-common fsl fslview afni itksnap mricron spm8-common spm8-data spm8-doc ia32-libs libc6-i386 galternatives midori qt4-qtconfig mesa-utils geany
+
+# install neuroimaging apps:
+apt-get -y install fsl fslview afni itksnap mricron spm8-common spm8-data spm8-doc   
+
+# get rid of cruft:
 apt-get -y remove update-notifier-core
 
