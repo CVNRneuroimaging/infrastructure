275bd8aab194daafe5c273478e262ff71eb28437
RM <rrmm@users.noreply.github.com>
2014-03-27 11:47:17 -0400

    -added crontab entry for every 2 minutes -ensure xservers are spawned before proceeding -ensure xserver shutdown under more conditions -use user's ssh private key -check for a file /etc/x2gousers/$USER before continuing

--- config/x2go-start-local.sh
+++ config/x2go-start-local.sh
@@ -1,9 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
-set -x
+#set -x
 
-USER=
-PASSWORD=
+USER=`whoami`
+SSH_PRIVKEY=/home/$USER/.ssh/id_rsa
+#PASSWORD=
 COMMAND=LXDE
 SESSION_TIMEOUT=120
 
@@ -11,6 +12,13 @@ SESSION_PID=`x2golistsessions| cut -f 1 -d "|"`
 if [ x"$SESSION_PID" != "x" ]; then
     echo "session exists", $SESSION_PID
     exit
+fi
+
+# check if user is permitted ... 
+# this is for admin convenience, not security
+if [ ! -e "/etc/x2gousers/$USER" ]; then
+    echo "user unknown or not permitted"
+    exit 1
 fi
 
 
@@ -22,9 +30,31 @@ XVFB_PID=$!
 DISPLAY=$XVFB_DISPLAY Xephyr -screen  800x600x24  $XEPHYR_DISPLAY &
 XEPHYR_PID=$!
 
+
+function terminate_xservers  {
+    /bin/kill -9 $XVFB_PID
+    /bin/kill -9 $XEPHYR_PID
+}
+
+
+
+sleep 1
+# make sure we started up
+/bin/kill -SIGWINCH $XVFB_PID
+RES1=$?
+/bin/kill -SIGWINCH $XEPHYR_PID
+RES2=$?
+
+if [ 0 -ne $RES1 ] || [ 0 -ne  $RES2 ]; then
+    echo "Couldn't start x servers"
+    # cleanup if only one started
+    terminate_xservers
+    exit 1
+fi
+
 START_TIME=`date +"%s"`
 
-DISPLAY=$XEPHYR_DISPLAY pyhoca-cli --server localhost -u $USER --password $PASSWORD -t desktop -c $COMMAND &
+DISPLAY=$XEPHYR_DISPLAY pyhoca-cli --server localhost -u $USER -t desktop -c $COMMAND -k $SSH_PRIVKEY & 
 CLI_PID=$!
 
 
@@ -32,21 +62,22 @@ while [ true ]; do
     NOW_TIME=`date +"%s"`
     SESSION_PID=`x2golistsessions| cut -f 1 -d "|"`
     ELAPSED_TIME=`expr $NOW_TIME - $START_TIME`
-    echo $SESSION_PID
+    #echo $SESSION_PID
     if [ x"$SESSION_PID" != "x" ]; then
-		echo "session started", $SESSION_PID
-		break
+	echo "session started", $SESSION_PID
+	break
     fi
     if [ $ELAPSED_TIME -gt $SESSION_TIMEOUT ]; then
-		echo "session timed out before starting"
-		break
+	echo "session timed out before starting"
+	break
     fi
     sleep 5
 done
 
 
 /bin/kill -SIGTERM $CLI_PID
-/bin/kill -SIGKILL  $XVFB_PID
+terminate_xservers
+
 
 
 
